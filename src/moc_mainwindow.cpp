5a1ff8b9d73dd2632b63b4f328b7a600a8d8382a
Austin Blanco <35380024+AdvancedResearchConsulting@users.noreply.github.com>
2018-02-01 13:54:22 -0800

    Source files uploaded
    ...where i want them

---
+++ src/moc_mainwindow.cpp
@@ -0,0 +1,209 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.10.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../terminal/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.10.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[27];
+    char stringdata0[509];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 14), // "openSerialPort"
+QT_MOC_LITERAL(2, 26, 0), // ""
+QT_MOC_LITERAL(3, 27, 15), // "closeSerialPort"
+QT_MOC_LITERAL(4, 43, 5), // "about"
+QT_MOC_LITERAL(5, 49, 9), // "writeData"
+QT_MOC_LITERAL(6, 59, 4), // "data"
+QT_MOC_LITERAL(7, 64, 8), // "readData"
+QT_MOC_LITERAL(8, 73, 11), // "handleError"
+QT_MOC_LITERAL(9, 85, 28), // "QSerialPort::SerialPortError"
+QT_MOC_LITERAL(10, 114, 5), // "error"
+QT_MOC_LITERAL(11, 120, 23), // "on_ttl_button_1_clicked"
+QT_MOC_LITERAL(12, 144, 23), // "on_ttl_button_2_clicked"
+QT_MOC_LITERAL(13, 168, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(14, 190, 23), // "on_ttl_button_3_clicked"
+QT_MOC_LITERAL(15, 214, 23), // "on_ttl_button_4_clicked"
+QT_MOC_LITERAL(16, 238, 23), // "on_ttl_button_5_clicked"
+QT_MOC_LITERAL(17, 262, 23), // "on_ttl_button_6_clicked"
+QT_MOC_LITERAL(18, 286, 23), // "on_ttl_button_7_clicked"
+QT_MOC_LITERAL(19, 310, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(20, 334, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(21, 358, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(22, 382, 26), // "on_cycleDelay_valueChanged"
+QT_MOC_LITERAL(23, 409, 5), // "value"
+QT_MOC_LITERAL(24, 415, 33), // "on_cycleDelayWindow_returnPre..."
+QT_MOC_LITERAL(25, 449, 30), // "on_slider_dac_1_sliderReleased"
+QT_MOC_LITERAL(26, 480, 28) // "on_slider_dac_1_valueChanged"
+
+    },
+    "MainWindow\0openSerialPort\0\0closeSerialPort\0"
+    "about\0writeData\0data\0readData\0handleError\0"
+    "QSerialPort::SerialPortError\0error\0"
+    "on_ttl_button_1_clicked\0on_ttl_button_2_clicked\0"
+    "on_pushButton_clicked\0on_ttl_button_3_clicked\0"
+    "on_ttl_button_4_clicked\0on_ttl_button_5_clicked\0"
+    "on_ttl_button_6_clicked\0on_ttl_button_7_clicked\0"
+    "on_pushButton_2_clicked\0on_pushButton_3_clicked\0"
+    "on_pushButton_4_clicked\0"
+    "on_cycleDelay_valueChanged\0value\0"
+    "on_cycleDelayWindow_returnPressed\0"
+    "on_slider_dac_1_sliderReleased\0"
+    "on_slider_dac_1_valueChanged"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      21,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  119,    2, 0x08 /* Private */,
+       3,    0,  120,    2, 0x08 /* Private */,
+       4,    0,  121,    2, 0x08 /* Private */,
+       5,    1,  122,    2, 0x08 /* Private */,
+       7,    0,  125,    2, 0x08 /* Private */,
+       8,    1,  126,    2, 0x08 /* Private */,
+      11,    0,  129,    2, 0x08 /* Private */,
+      12,    0,  130,    2, 0x08 /* Private */,
+      13,    0,  131,    2, 0x08 /* Private */,
+      14,    0,  132,    2, 0x08 /* Private */,
+      15,    0,  133,    2, 0x08 /* Private */,
+      16,    0,  134,    2, 0x08 /* Private */,
+      17,    0,  135,    2, 0x08 /* Private */,
+      18,    0,  136,    2, 0x08 /* Private */,
+      19,    0,  137,    2, 0x08 /* Private */,
+      20,    0,  138,    2, 0x08 /* Private */,
+      21,    0,  139,    2, 0x08 /* Private */,
+      22,    1,  140,    2, 0x08 /* Private */,
+      24,    0,  143,    2, 0x08 /* Private */,
+      25,    0,  144,    2, 0x08 /* Private */,
+      26,    1,  145,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QByteArray,    6,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 9,   10,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   23,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   23,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->openSerialPort(); break;
+        case 1: _t->closeSerialPort(); break;
+        case 2: _t->about(); break;
+        case 3: _t->writeData((*reinterpret_cast< const QByteArray(*)>(_a[1]))); break;
+        case 4: _t->readData(); break;
+        case 5: _t->handleError((*reinterpret_cast< QSerialPort::SerialPortError(*)>(_a[1]))); break;
+        case 6: _t->on_ttl_button_1_clicked(); break;
+        case 7: _t->on_ttl_button_2_clicked(); break;
+        case 8: _t->on_pushButton_clicked(); break;
+        case 9: _t->on_ttl_button_3_clicked(); break;
+        case 10: _t->on_ttl_button_4_clicked(); break;
+        case 11: _t->on_ttl_button_5_clicked(); break;
+        case 12: _t->on_ttl_button_6_clicked(); break;
+        case 13: _t->on_ttl_button_7_clicked(); break;
+        case 14: _t->on_pushButton_2_clicked(); break;
+        case 15: _t->on_pushButton_3_clicked(); break;
+        case 16: _t->on_pushButton_4_clicked(); break;
+        case 17: _t->on_cycleDelay_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 18: _t->on_cycleDelayWindow_returnPressed(); break;
+        case 19: _t->on_slider_dac_1_sliderReleased(); break;
+        case 20: _t->on_slider_dac_1_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 21)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 21;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 21)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 21;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
